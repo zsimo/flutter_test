d6c9f22eb255b5243654fc2859b084d2157d38d7
Simone Sacchi <simone.sacchi@nubilaria.com>
2024-03-01 17:36:19 +0100

    feat: base working app

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: WebViewApp(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
